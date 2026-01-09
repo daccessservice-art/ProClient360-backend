d2397d113bc17e3ce0e0841667b32b87111eff97
Nilkanth <nilkanthpokharkar@gmail.com>
2026-01-09 17:10:36 +0530

    firebase updated code

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,6 +1,13 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json'); 
 
+// 1. Get the service account data from an Environment Variable
+// On Render, we will provide this as a string
+const serviceAccountVar = process.env.FIREBASE_SERVICE_ACCOUNT;
+
+// 2. Parse the string into an object, or fall back to the local file (for local dev)
+const serviceAccount = serviceAccountVar 
+    ? JSON.parse(serviceAccountVar) 
+    : require('../serviceAccountKey.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
