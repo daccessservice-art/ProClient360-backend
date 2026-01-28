9c95c40f3c45297ddb89d6d4ad6ad8c518c9197d
Nilkanth <nilkanthpokharkar@gmail.com>
2026-01-28 10:42:26 +0530

    customer routes update

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -3,8 +3,15 @@ const { permissionMiddleware, isCompany } = require('../middlewares/auth');
 const { showAll, createCustomer, updateCustomer, deleteCustomer, getCustomer, exportCustomersPDF, exportCustomersExcel } = require('../controllers/customerController');
 const router = express.Router();
 
+// IMPORTANT: Export routes MUST come BEFORE parameterized routes like /:id
+// Otherwise Express treats 'export' as an ID parameter
+router.get('/export/pdf', permissionMiddleware(['viewCustomer']), exportCustomersPDF);
+router.get('/export/excel', permissionMiddleware(['viewCustomer']), exportCustomersExcel);
+
+// General routes
 router.get('/', permissionMiddleware(['viewCustomer']), showAll);
 
+// Parameterized routes come AFTER specific routes
 router.get('/:id', permissionMiddleware(['viewCustomer']), getCustomer);
 
 router.post('/', permissionMiddleware(['createCustomer']), createCustomer);
@@ -12,9 +19,5 @@ router.post('/', permissionMiddleware(['createCustomer']), createCustomer);
 router.put('/:id', permissionMiddleware(['updateCustomer']), updateCustomer);
 
 router.delete('/:id', permissionMiddleware(['deleteCustomer']), deleteCustomer);
-
-// Export routes
-router.get('/export/pdf', permissionMiddleware(['viewCustomer']), exportCustomersPDF);
-router.get('/export/excel', permissionMiddleware(['viewCustomer']), exportCustomersExcel);
 
 module.exports = router;
