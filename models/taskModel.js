965dddbec8a78129f438d229039f21e092ef9e0e
Nilkanth <nilkanthpokharkar@gmail.com>
2026-01-30 12:51:05 +0530

    add length in remark field 200 to 2000

--- models/taskModel.js
+++ models/taskModel.js
@@ -8,7 +8,7 @@ const taskSchema = new Schema({
     required: [true, 'Task name is required'],
     unique: [true, 'A task with this name already exists'],
     trim: true,
-    maxlength: [200, 'Task name cannot exceed 200 characters'],
+    maxlength: [2000, 'Task name cannot exceed 2000 characters'],
   },
   company:{
     type:mongoose.Schema.Types.ObjectId,
